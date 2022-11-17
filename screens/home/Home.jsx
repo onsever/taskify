d100558caa6af2897824d5671ac6c1979e10f23c
Onurcan Sever <onsever@icloud.com>
2022-11-16 20:13:45 -0500

    created Home screen

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,17 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 
-const Details = ({ navigation, route }) => {
+const Home = ({ navigation, route }) => {
   const { values } = route.params;
 
   return (
-    <SafeAreaView>
-      <View className="flex items-center justify-center w-screen h-screen">
-        <Text>Email: {values.email}</Text>
-        <Text>Password: {values.password}</Text>
-      </View>
+    <SafeAreaView className={"w-screen h-screen bg-primary"}>
+        <StatusBar barStyle="light-content" />
+        <View className="flex items-center justify-center">
+            <Text>Email: {values.email}</Text>
+            <Text>Password: {values.password}</Text>
+        </View>
     </SafeAreaView>
   );
 };
 
-export default Details;
+export default Home;
