fa47799d3c32e8e6f5200b7155410ec10461c4ef
Onurcan Sever <onsever@icloud.com>
2022-11-17 09:58:36 -0500

    modified home screen

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,15 +1,13 @@
 import { View, Text, StatusBar } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
+import { Agenda } from "react-native-calendars/src/index";
 
-const Home = ({ navigation, route }) => {
-  const { values } = route.params;
-
+const Home = ({ navigation }) => {
   return (
     <SafeAreaView className={"w-screen h-screen bg-primary"}>
         <StatusBar barStyle="light-content" />
         <View className="flex items-center justify-center">
-            <Text>Email: {values.email}</Text>
-            <Text>Password: {values.password}</Text>
+            <Text className="text-white text-2xl font-bold">Home Page</Text>
         </View>
     </SafeAreaView>
   );
