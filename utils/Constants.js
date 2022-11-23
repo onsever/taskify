0a727f92ef6b019bdd8839318216fcfa60d9182e
Nisha-Bhattarai <c0840054@mylambton.ca>
2022-11-22 23:30:42 -0500

    Bug fixes

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,26 +1,26 @@
 import uuid from "react-uuid";
 
 class Task {
-    constructor(name, description, startDate, endDate, completed) {
-        this.name = name;
-        this.description = description;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.completed = completed;
-        this.id = uuid();
-    }
+  constructor(name, description, startDate, endDate, completed) {
+    this.name = name;
+    this.description = description;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.completed = completed;
+    this.id = uuid();
+  }
 
-    setTeamMembers(teamMembers) {
-        this.teamMembers = teamMembers;
-    }
+  setTeamMembers(teamMembers) {
+    this.teamMembers = teamMembers;
+  }
 }
 
 export default class Constants {
-    constructor() {}
-    static dummyData = [
-        new Task("Task 1", "Description 1", "2022-10-18", "2021-10-02", false),
-        new Task("Task 2", "Description 2", "2022-10-18", "2021-10-02", false),
-        new Task("Task 3", "Description 3", "2022-10-18", "2021-10-02", false),
-        new Task("Task 4", "Description 4", "2022-10-19", "2021-10-02", false),
-    ];
+  constructor() {}
+  static dummyData = [
+    new Task("Task 1", "Description 1", "2022-11-24", "2022-11-02", false),
+    new Task("Task 2", "Description 2", "2022-11-23", "2022-11-02", false),
+    new Task("Task 3", "Description 3", "2022-11-26", "2022-11-02", false),
+    new Task("Task 4", "Description 4", "2022-11-24", "2022-11-02", false),
+  ];
 }
