12f63b5c072ec3f08174e492f13c9b253b832815
Roch Bajracharya <bajracharyaroch@gmail.com>
2022-11-28 18:45:22 -0500

    ongoing task view

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,20 +1,29 @@
-import { View, Text, StatusBar, Pressable, Image, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  StatusBar,
+  Pressable,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
-import { Agenda } from "react-native-calendars/src/index";
 import { SearchBar } from "react-native-elements";
 import Colors from "../../utils/Colors";
 import { Ionicons } from "@expo/vector-icons";
-import ActionButton from "../../components/ActionButton";
 import { ScrollView } from "react-native-gesture-handler";
 import { useFetch } from "../../hooks/useFetch";
 import { useEffect } from "react";
+import moment from "moment";
 
 const Home = ({ navigation }) => {
   const { fetch, result, loaded, loading, error } = useFetch();
+  const ongoingTaskFetch = useFetch();
 
   useEffect(() => {
-    fetch('project/user');
-  }, [])
+    fetch("project/user");
+    ongoingTaskFetch.fetch("task/ongoing");
+  }, []);
 
   return (
     <SafeAreaView className={"w-screen h-screen bg-primary p-8"}>
@@ -55,16 +64,22 @@ const Home = ({ navigation }) => {
         </View>
         <ScrollView horizontal>
           <View className={"flex flex-row items-center mt-5 mb-5"}>
-            {loading ? <ActivityIndicator /> : (result?.map(x => {
-              return (
-                <View className={"p-5 bg-secondary rounded-lg mr-2 ml-2"}>
-                  <Text className={"fw-bold text-white text-xl mb-3"}>
-                    {x.title}
-                  </Text>
-                  <Text className={"text-white text-l"}>{x.tasks.length}</Text>
-                </View>
-              )
-            }))}
+            {loading ? (
+              <ActivityIndicator />
+            ) : (
+              result?.map((x) => {
+                return (
+                  <View className={"p-5 bg-secondary rounded-lg mr-2 ml-2"}>
+                    <Text className={"fw-bold text-white text-xl mb-3"}>
+                      {x.title}
+                    </Text>
+                    <Text className={"text-white text-l"}>
+                      {x.tasks.length}
+                    </Text>
+                  </View>
+                );
+              })
+            )}
           </View>
         </ScrollView>
         <View className={"mb-40"}>
@@ -76,135 +91,76 @@ const Home = ({ navigation }) => {
           </View>
 
           <View className={"flex items-center justify-center mt-5"}>
-            <View className={"p-5 bg-secondary rounded-lg w-full m-5"}>
-              <View
-                className={"flex flex-row justify-between items-center mb-3"}
-              >
-                <Text className={"fw-bold text-white text-xl "}>Project 1</Text>
-                <View className={"bg-anakiwa rounded-full"}>
-                  <Text className={"text-white p-3 "}>6d</Text>
-                </View>
-              </View>
-              <Text className={"text-white text-l"}>Team Members</Text>
-              <View className={"flex flex-row"}>
-                <View className={"mt-4 mr-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://www.nicepng.com/png/detail/388-3889577_girl-vector-3-an-images-hub-girl-vector.png",
+            {ongoingTaskFetch.loading ? (
+              <ActivityIndicator />
+            ) : (
+              ongoingTaskFetch.result.map((task) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("");
                     }}
-                  />
-                </View>
-                <View className={"mt-4 mr-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://thumbs.dreamstime.com/b/beautiful-asian-woman-smile-vector-illustration-beautiful-woman-specs-wspecs-wispecs-witspecs-withspecs-vector-illustration-134357609.jpg",
-                    }}
-                  />
-                </View>
-                <View className={"mt-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://www.pngitem.com/pimgs/m/4-42408_vector-art-design-men-fashion-vector-art-illustration.png",
-                    }}
-                  />
-                </View>
-              </View>
-              <View className={"mt-5 flex flex-row items-center"}>
-                <Ionicons name="time" size={26} color={Colors.bittersweet} />
-                <Text className={"text-white ml-5 text-md"}>
-                  2:30 PM - 6: 00 PM
-                </Text>
-              </View>
-            </View>
-            <View className={"p-5 bg-secondary rounded-lg w-full m-5"}>
-              <View
-                className={"flex flex-row justify-between items-center mb-3"}
-              >
-                <Text className={"fw-bold text-white text-xl "}>Project 2</Text>
-                <View className={"bg-anakiwa rounded-full"}>
-                  <Text className={"text-white p-3 "}>6d</Text>
-                </View>
-              </View>
-              <Text className={"text-white text-l"}>Team Members</Text>
-              <View className={"flex flex-row"}>
-                <View className={"mt-4 mr-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://www.nicepng.com/png/detail/388-3889577_girl-vector-3-an-images-hub-girl-vector.png",
-                    }}
-                  />
-                </View>
-                <View className={"mt-4 mr-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://thumbs.dreamstime.com/b/beautiful-asian-woman-smile-vector-illustration-beautiful-woman-specs-wspecs-wispecs-witspecs-withspecs-vector-illustration-134357609.jpg",
-                    }}
-                  />
-                </View>
-                <View className={"mt-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://www.pngitem.com/pimgs/m/4-42408_vector-art-design-men-fashion-vector-art-illustration.png",
-                    }}
-                  />
-                </View>
-              </View>
-              <View className={"mt-5 flex flex-row items-center"}>
-                <Ionicons name="time" size={26} color={Colors.bittersweet} />
-                <Text className={"text-white ml-5 text-md"}>
-                  2:30 PM - 6: 00 PM
-                </Text>
-              </View>
-            </View>
-            <View className={"p-5 bg-secondary rounded-lg w-full m-5"}>
-              <View
-                className={"flex flex-row justify-between items-center mb-3"}
-              >
-                <Text className={"fw-bold text-white text-xl "}>Project 3</Text>
-                <View className={"bg-anakiwa rounded-full"}>
-                  <Text className={"text-white p-3 "}>6d</Text>
-                </View>
-              </View>
-              <Text className={"text-white text-l"}>Team Members</Text>
-              <View className={"flex flex-row"}>
-                <View className={"mt-4 mr-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://www.nicepng.com/png/detail/388-3889577_girl-vector-3-an-images-hub-girl-vector.png",
-                    }}
-                  />
-                </View>
-                <View className={"mt-4 mr-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://thumbs.dreamstime.com/b/beautiful-asian-woman-smile-vector-illustration-beautiful-woman-specs-wspecs-wispecs-witspecs-withspecs-vector-illustration-134357609.jpg",
-                    }}
-                  />
-                </View>
-                <View className={"mt-4"}>
-                  <Image
-                    className={"w-10 h-10 rounded-full"}
-                    source={{
-                      uri: "https://www.pngitem.com/pimgs/m/4-42408_vector-art-design-men-fashion-vector-art-illustration.png",
-                    }}
-                  />
-                </View>
-              </View>
-              <View className={"mt-5 flex flex-row items-center"}>
-                <Ionicons name="time" size={26} color={Colors.bittersweet} />
-                <Text className={"text-white ml-5 text-md"}>
-                  2:30 PM - 6: 00 PM
-                </Text>
-              </View>
-            </View>
+                    className={"p-5 bg-secondary rounded-lg w-full m-5"}
+                  >
+                    <View
+                      className={
+                        "flex flex-row justify-between items-center mb-3"
+                      }
+                    >
+                      <Text className={"fw-bold text-white text-xl "}>
+                        {task.title}
+                      </Text>
+                      <View className={"bg-anakiwa rounded-full"}>
+                        <Text className={"text-white p-3 "}>
+                          {moment(task.endDate, "DD/MM/YYYY").diff(
+                            moment(new Date()),
+                            "days"
+                          )}
+                          D
+                        </Text>
+                      </View>
+                    </View>
+                    {/* <Text className={"text-white text-l"}>Team Members</Text>
+                    <View className={"flex flex-row"}>
+                      <View className={"mt-4 mr-4"}>
+                        <Image
+                          className={"w-10 h-10 rounded-full"}
+                          source={{
+                            uri: "https://www.nicepng.com/png/detail/388-3889577_girl-vector-3-an-images-hub-girl-vector.png",
+                          }}
+                        />
+                      </View>
+                      <View className={"mt-4 mr-4"}>
+                        <Image
+                          className={"w-10 h-10 rounded-full"}
+                          source={{
+                            uri: "https://thumbs.dreamstime.com/b/beautiful-asian-woman-smile-vector-illustration-beautiful-woman-specs-wspecs-wispecs-witspecs-withspecs-vector-illustration-134357609.jpg",
+                          }}
+                        />
+                      </View>
+                      <View className={"mt-4"}>
+                        <Image
+                          className={"w-10 h-10 rounded-full"}
+                          source={{
+                            uri: "https://www.pngitem.com/pimgs/m/4-42408_vector-art-design-men-fashion-vector-art-illustration.png",
+                          }}
+                        />
+                      </View>
+                    </View> */}
+                    <View className={"mt-5 flex flex-row items-center"}>
+                      <Ionicons
+                        name="time"
+                        size={26}
+                        color={Colors.bittersweet}
+                      />
+                      <Text className={"text-white ml-5 text-md"}>
+                        {task.startDate} - {task.endDate}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                );
+              })
+            )}
           </View>
         </View>
       </ScrollView>
